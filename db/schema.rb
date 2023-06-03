852f9f84dd288f75d22ac87692e4f7b9c106238a
linlin <1208215066@qq.com>
2023-06-03 15:34:34 +0800

    创建了表，创建了实现delayed_job的脚本，获得了Delayed生成的文件

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_03_054132) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_03_060028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_03_054132) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
+
+  create_table "myfiles", force: :cascade do |t|
+    t.string "name"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
